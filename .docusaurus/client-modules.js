314043bde19a66731bde3549c49121d43091dcab
mdraselmiah95 <russelroy15@gmail.com>
2025-07-24 14:25:59 +0600

    new doc project added

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/Users/raselmiah/Developer/Programming/Other-Project/optimistic-doc/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/Users/raselmiah/Developer/Programming/Other-Project/optimistic-doc/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/raselmiah/Developer/Programming/Other-Project/optimistic-doc/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/raselmiah/Developer/Programming/Other-Project/optimistic-doc/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/raselmiah/Developer/Programming/Other-Project/optimistic-doc/src/css/custom.css"),
+];
